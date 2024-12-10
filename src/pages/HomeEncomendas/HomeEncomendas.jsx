b1c8f3d04da03154b067111bf0c92493e77c5acb
nandadosoob <fer.r.benetti@gmail.com>
2024-12-10 15:23:36 -0300

    eu vou me matar

--- src/pages/HomeEncomendas/HomeEncomendas.jsx
+++ src/pages/HomeEncomendas/HomeEncomendas.jsx
@@ -1,80 +1,160 @@
 import { useState } from "react";
 import { Pagina } from "../../components/Pagina";
 import { Link } from "react-router-dom";
+import { use } from "react";
 
 export function HomeEncomendas() {
-  const encomendas = [
-    {
-      id: 1,
-      cliente: "Heber Stein Mazutti",
-      estilista: "Flávia Cristina",
-      encomenda: "Vestido Sob Medida",
-      dataHora: "01/10/2024 - 08:30",
-      valor: "R$ 500,00",
-    },
-    {
-      id: 2,
-      cliente: "Marcos Antunes",
-      estilista: "Ana Beatriz Rutini",
-      encomenda: "Terno Social",
-      dataHora: "01/10/2024 - 10:00",
-      valor: "R$ 700,00",
-    },
-    {
-      id: 3,
-      cliente: "Joana Silveira Araújo",
-      estilista: "Nise da Silveira",
-      encomenda: "Blusa Bordada",
-      dataHora: "02/10/2024 - 11:45",
-      valor: "R$ 300,00",
-    },
-    {
-      id: 4,
-      cliente: "Marcelo Bueno",
-      estilista: "Sandra Rocha",
-      encomenda: "Calça Casual",
-      dataHora: "02/10/2024 - 15:00",
-      valor: "R$ 400,00",
-    },
-    {
-      id: 5,
-      cliente: "Solange Carvalho",
-      estilista: "Sandra Rocha",
-      encomenda: "Vestido Sereia",
-      dataHora: "03/10/2024 - 17:30",
-      valor: "R$ 200,00",
-    },
-  ];
+  // const encomendas = [
+  //   {
+  //     id: 1,
+  //     cliente: "Heber Stein Mazutti",
+  //     estilista: "Flávia Cristina",
+  //     encomenda: "Vestido Sob Medida",
+  //     dataHora: "01/10/2024 - 08:30",
+  //     valor: "R$ 500,00",
+  //   },
+  //   {
+  //     id: 2,
+  //     cliente: "Marcos Antunes",
+  //     estilista: "Ana Beatriz Rutini",
+  //     encomenda: "Terno Social",
+  //     dataHora: "01/10/2024 - 10:00",
+  //     valor: "R$ 700,00",
+  //   },
+  //   {
+  //     id: 3,
+  //     cliente: "Joana Silveira Araújo",
+  //     estilista: "Nise da Silveira",
+  //     encomenda: "Blusa Bordada",
+  //     dataHora: "02/10/2024 - 11:45",
+  //     valor: "R$ 300,00",
+  //   },
+  //   {
+  //     id: 4,
+  //     cliente: "Marcelo Bueno",
+  //     estilista: "Sandra Rocha",
+  //     encomenda: "Calça Casual",
+  //     dataHora: "02/10/2024 - 15:00",
+  //     valor: "R$ 400,00",
+  //   },
+  //   {
+  //     id: 5,
+  //     cliente: "Solange Carvalho",
+  //     estilista: "Sandra Rocha",
+  //     encomenda: "Vestido Sereia",
+  //     dataHora: "03/10/2024 - 17:30",
+  //     valor: "R$ 200,00",
+  //   },
+  // ];
 
-  const [filteredEncomendas, setFilteredEncomendas] = useState(encomendas);
-  const [dataInicio, setDataInicio] = useState('');
-  const [dataFim, setDataFim] = useState('');
-  const [estilista, setEstilista] = useState('');
+  // const [filteredEncomendas, setFilteredEncomendas] = useState(encomendas);
+  // const [dataInicio, setDataInicio] = useState('');
+  // const [dataFim, setDataFim] = useState('');
+  // const [estilista, setEstilista] = useState('');
 
-  const handleFilter = () => {
-    let filtered = encomendas;
+  // const handleFilter = () => {
+  //   let filtered = encomendas;
 
-    const formatDate = (dateStr) => {
-      const [day, month, year] = dateStr.split(' - ')[0].split('/');
-      return new Date(`${year}-${month}-${day}`); // Convertendo para Date
+  //   const formatDate = (dateStr) => {
+  //     const [day, month, year] = dateStr.split(' - ')[0].split('/');
+  //     return new Date(`${year}-${month}-${day}`); // Convertendo para Date
+  //   };
+
+  //   if (dataInicio) {
+  //     const dataInicioFormatted = new Date(dataInicio);
+  //     filtered = filtered.filter(encomenda => formatDate(encomenda.dataHora) >= dataInicioFormatted);
+  //   }
+
+  //   if (dataFim) {
+  //     const dataFimFormatted = new Date(dataFim);
+  //     filtered = filtered.filter(encomenda => formatDate(encomenda.dataHora) <= dataFimFormatted);
+  //   }
+
+  //   if (estilista) {
+  //     filtered = filtered.filter(encomenda => encomenda.estilista === estilista);
+  //   }
+
+  //   setFilteredEncomendas(filtered);
+  // };
+
+  const [cliente, setCliente] = useState("")
+  const [estilista, setEstilista] = useState("")
+  const [tipoEncomenda, setTipoEncomenda] = useState("")
+  const [horarioPedido, setHorarioPedido] = useState("")
+  const [valor, setValor] = useState("")
+
+  const [criaPedido, setCriaPedido] = useState(false);
+
+  const adicionaCriaPedido = () => {
+  
+
+    return (
+
+   
+        <div className="text-gray-700 border-b border-gray-200 hover:bg-gray-100 flex ">
+
+        
+          <div className="py-4 px-4 w-16.6"><input onChange={(event) => setCliente(event.target.value)}
+            value={cliente} /></div>
+          <div className="py-4 px-4 w-16.6"><input onChange={(event) => setEstilista(event.target.value)}
+            value={estilista} /></div>
+          <div className="py-4 px-4 w-16.6"><input onChange={(event) => setTipoEncomenda(event.target.value)}
+            value={tipoEncomenda} /></div>
+          <div className="py-4 px-4 w-16.6"><input onChange={(event) => setHorarioPedido(event.target.value)}
+            value={horarioPedido} /></div>
+          <div className="py-4 px-4 w-16.6"><input onChange={(event) => setValor(event.target.value)}
+            value={valor} /></div>
+          <div>
+            <button>Registrar pedido</button>
+          </div>
+        
+        </div>
+    )
+  }
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const encomendas = {
+      cliente,
+      estilista,
+      tipoEncomenda,
+      horarioPedido,
+      valor
     };
 
-    if (dataInicio) {
-      const dataInicioFormatted = new Date(dataInicio);
-      filtered = filtered.filter(encomenda => formatDate(encomenda.dataHora) >= dataInicioFormatted);
-    }
 
-    if (dataFim) {
-      const dataFimFormatted = new Date(dataFim);
-      filtered = filtered.filter(encomenda => formatDate(encomenda.dataHora) <= dataFimFormatted);
-    }
+    try {
+      const resposta = await fetch("https://final-project-dw2.onrender.com/encomendas", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(encomendas)
+      });
 
-    if (estilista) {
-      filtered = filtered.filter(encomenda => encomenda.estilista === estilista);
-    }
+      if (resposta.status === 201) {
+        console.log('passei')
+        alert("Cadastro concluído com sucesso!!");
 
-    setFilteredEncomendas(filtered);
-  };
+        setCliente("");
+        setEstilista("");
+        setTipoEncomenda("");
+        setHorarioPedido("");
+        setValor("");
+      } else {
+        alert(`Erro ao cadastrar: ${resposta.status}`);
+      }
+
+      //A ok propriedade somente leitura da Responseinterface contém um booleano informando se a resposta foi bem-sucedida (status no intervalo 200-299) ou não. Valor booleano
+    } catch (error) {
+      console.log("Erro ao conectar c servidor:", error);
+      alert("Erro ao conectar com o servidor.")
+    };
+  }
+
+
 
   return (
     <Pagina>
@@ -105,7 +185,9 @@ export function HomeEncomendas() {
           <div className="flex items-center justify-between mb-6 flex-wrap">
             <div className="flex items-center space-x-4">
               <h1 className="text-2xl font-bold">Encomendas</h1>
-              <button className="px-3 py-2 bg-white text-pink-800 rounded-md hover:bg-gray-200 text-lg font-semibold border border-pink-800">
+              <button className="px-3 py-2 bg-white text-pink-800 rounded-md hover:bg-gray-200 text-lg font-semibold border border-pink-800"
+                >
+
                 Nova Encomenda
               </button>
             </div>
@@ -113,20 +195,20 @@ export function HomeEncomendas() {
             <div className="flex items-center space-x-4">
               <input
                 type="date"
-                value={dataInicio}
-                onChange={(e) => setDataInicio(e.target.value)}
+                // value={dataInicio}
+                // onChange={(e) => setDataInicio(e.target.value)}
                 className="border border-gray-300 rounded-md px-4 py-2 text-lg focus:outline-none focus:ring focus:ring-gray-200"
               />
               <span className="text-gray-600">Até</span>
               <input
                 type="date"
-                value={dataFim}
-                onChange={(e) => setDataFim(e.target.value)}
+                // value={dataFim}
+                // onChange={(e) => setDataFim(e.target.value)}
                 className="border border-gray-300 rounded-md px-4 py-2 text-lg focus:outline-none focus:ring focus:ring-gray-200"
               />
               <select
-                value={estilista}
-                onChange={(e) => setEstilista(e.target.value)}
+                // value={estilista}
+                // onChange={(e) => setEstilista(e.target.value)}
                 className="border border-gray-300 rounded-md px-4 py-2 text-lg focus:outline-none focus:ring focus:ring-gray-200"
               >
                 <option value="">Todos os estilistas</option>
@@ -140,7 +222,7 @@ export function HomeEncomendas() {
                 <option value="Sandra Rocha">Sandra Rocha</option>
               </select>
               <button
-                onClick={handleFilter}
+                // onClick={handleFilter}
                 className="px-5 py-2 bg-pink-700 text-white rounded-md hover:bg-pink-800 text-lg font-semibold"
               >
                 Filtrar
@@ -160,29 +242,39 @@ export function HomeEncomendas() {
                 <th className="py-4 px-4 text-center">Ações</th>
               </tr>
             </thead>
-            <tbody className="text-gray-700">
-              {filteredEncomendas.map((encomenda) => (
-                <tr
+
+            {/* <tbody className="text-gray-700"> */}
+            {/* {filteredEncomendas.map((encomenda) => ( */}
+            {/* <tr
                   key={encomenda.id}
                   className="border-b border-gray-200 hover:bg-gray-100"
-                >
-                  <td className="py-4 px-4">{encomenda.cliente}</td>
+                > */}
+            {/* <td className="py-4 px-4">{encomenda.cliente}</td>
                   <td className="py-4 px-4">{encomenda.estilista}</td>
                   <td className="py-4 px-4">{encomenda.encomenda}</td>
                   <td className="py-4 px-4">{encomenda.dataHora}</td>
-                  <td className="py-4 px-4">{encomenda.valor}</td>
-                  <td className="py-4 px-4 text-center">
+                  <td className="py-4 px-4">{encomenda.valor}</td> */}
+            {/* <td className="py-4 px-4 text-center">
                     <button className="bg-pink-800 text-white hover:bg-pink-900 px-3 py-2 rounded-md mr-2">
                       ✎
                     </button>
                     <button className="bg-gray-400 text-white hover:bg-gray-500 px-3 py-2 rounded-md">
                       🗑
                     </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
+                  </td> */}
+            {/* </tr> */}
+            {/* ))} */}
+            {/* </tbody> */}
           </table>
+          <div className="text-gray-700 border-b border-gray-200 hover:bg-gray-100 flex justify-evenly">
+            <div className="py-4 px-4 "></div>
+            <div className="py-4 px-4 "></div>
+            <div className="py-4 px-4 "></div>
+            <div className="py-4 px-4 "></div>
+            <div className="py-4 px-4 "></div>
+            <div className="py-4 px-4 "></div>
+          </div>
+
         </div>
       </div>
     </Pagina>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '16.6': '16,6%',
+
+      }
+    },
   },
   plugins: [],
 }
