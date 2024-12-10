046e7a4db8941c8cd2764a6ccc2ce6ab4cf6359e
nandadosoob <fer.r.benetti@gmail.com>
2024-12-10 19:39:09 -0300

    eu nao aguento mais

--- src/pages/HomeEncomendas/HomeEncomendas.jsx
+++ src/pages/HomeEncomendas/HomeEncomendas.jsx
@@ -83,34 +83,6 @@ export function HomeEncomendas() {
   const [horarioPedido, setHorarioPedido] = useState("")
   const [valor, setValor] = useState("")
 
-  const [criaPedido, setCriaPedido] = useState(false);
-
-  const adicionaCriaPedido = () => {
-  
-
-    return (
-
-   
-        <div className="text-gray-700 border-b border-gray-200 hover:bg-gray-100 flex ">
-
-        
-          <div className="py-4 px-4 w-16.6"><input onChange={(event) => setCliente(event.target.value)}
-            value={cliente} /></div>
-          <div className="py-4 px-4 w-16.6"><input onChange={(event) => setEstilista(event.target.value)}
-            value={estilista} /></div>
-          <div className="py-4 px-4 w-16.6"><input onChange={(event) => setTipoEncomenda(event.target.value)}
-            value={tipoEncomenda} /></div>
-          <div className="py-4 px-4 w-16.6"><input onChange={(event) => setHorarioPedido(event.target.value)}
-            value={horarioPedido} /></div>
-          <div className="py-4 px-4 w-16.6"><input onChange={(event) => setValor(event.target.value)}
-            value={valor} /></div>
-          <div>
-            <button>Registrar pedido</button>
-          </div>
-        
-        </div>
-    )
-  }
 
 
   const handleSubmit = async (event) => {
